2a6acfe588c47723ef4adeb02bb55903bdb28c63
Cem <cmbicakci@gmail.com>
2022-12-21 21:35:04 +0300

    createAsyncThunk

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,11 +1,17 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 import cartItems from "../../cartItems";
+
+const url = 'https://course-api.com/react-useReducer-cart-project';
+
+export const getCartItems = createAsyncThunk('cart/getCartItems', () => {
+    return fetch(url).then(res => res.json()).catch((err) => console.log(err))
+})
 
 const cartSlice = createSlice({
     name: 'cart',
     initialState: {
-        cartItems: cartItems,
+        cartItems: [],
         amount: 1,
         total: 0,
         isLoading: true
@@ -38,11 +44,22 @@ const cartSlice = createSlice({
             state.amount = amount;
             state.total = total;
         }
-
-
+    },
+    extraReducers: {
+        [getCartItems.pending]: (state) => {
+            state.isLoading = true;
+        },
+        [getCartItems.fulfilled]: (state, action) => {
+            console.log(action.payload)
+            state.isLoading = false;
+            state.cartItems = action.payload
+        },
+        [getCartItems.rejected]: (state) => {
+            state.isLoading = false;
+        }
     }
 
 })
 
-export const { clearCart, removeItem, increase, decrease, calculateTotals  } = cartSlice.actions
+export const { clearCart, removeItem, increase, decrease, calculateTotals } = cartSlice.actions
 export default cartSlice.reducer
